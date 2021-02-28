aa8c4ed2ee893dc6f6727d07f7ee3b6c264580cd
Giovanni <giovannitempestainfo@gmail.com>
2021-02-28 21:19:36 +0100

    setup operator lodout

--- app/src/main/java/com/example/r6strategies/OperatoreLoadout.java
+++ app/src/main/java/com/example/r6strategies/OperatoreLoadout.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.r6strategies.modelli.OperatoreSpot;
+
 public class OperatoreLoadout extends Fragment {
 
     public OperatoreLoadout() {
@@ -18,6 +20,94 @@ public class OperatoreLoadout extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operator_loadout, container, false);
+        View view = inflater.inflate(R.layout.fragment_operator_loadout, container, false);
+        Bundle bundle = getArguments();
+        OperatoreSpot os= (OperatoreSpot) bundle.getSerializable("os");
+        switch(os.operatore){
+            case"VALKYRIE":
+
+                break;
+            case"BANDIT":
+
+                break;
+            case"MUTE":
+
+                break;
+            case"SMOKE":
+
+                break;
+            case"MELUSI":
+
+                break;
+            case"MIRA":
+
+                break;
+            case"VIGIL":
+
+
+                break;
+            case"KAID":
+
+                break;
+            case"ARUNI":
+
+                break;
+            case"ALIBI":
+
+                break;
+            case"FROST":
+
+                break;
+            case"LESION":
+
+                break;
+            case"ELA":
+
+                break;
+            case"CAIVERA":
+
+                break;
+            case"GOYO":
+
+                break;
+            case"JAGER":
+
+                break;
+            case"MAESTRO":
+
+                break;
+            case"MOZZIE":
+
+                break;
+            case"ORYX":
+
+                break;
+            case"TACHANKA":
+
+                break;
+            case"WAMAI":
+
+                break;
+            case"PULSE":
+
+                break;
+            case"KAPKAN":
+
+                break;
+            case"DOC":
+
+                break;
+            case"ROOK":
+
+                break;
+            case"CASTLE":
+
+                break;
+            case"WARDEN":
+
+                break;
+        }
+        return view;
     }
+
 }
